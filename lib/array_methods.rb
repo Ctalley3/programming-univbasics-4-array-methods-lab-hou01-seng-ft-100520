588e248995c2e662aa7d864fa1374510dc86e8ba
Crystal T <ctalley.cat@gmail.com>
2020-09-23 19:34:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,15 +8,15 @@ array.sort(array)
 end
 
 def using_reverse(array)
-array.using_reverse
+array.using_reverse (array)
 end
 
 def using_first(array)
-array.
+array.using_first
 end
 
 def using_last(array)
-
+array.last
 end
 
 def using_size(array)
